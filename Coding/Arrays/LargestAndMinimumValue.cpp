b724d1e5990ac65d0a477b66465b39a0ab749951
anurag <90618167+BugRuster@users.noreply.github.com>
2023-02-09 10:52:47 +0530

    New Files Added

--- Coding/Arrays/LargestAndMinimumValue.cpp
+++ Coding/Arrays/LargestAndMinimumValue.cpp
@@ -11,10 +11,20 @@ int large(int arr[], int size) {
     return largest;
 }
 
+int mini(int arr[], int size) {
+    int minimum = INT_MAX; // Plus Infinity
+    for (int i = 0; i < size; i++) {
+        if (minimum > arr[i]) minimum = arr[i];
+    }
+    return minimum;
+}
 int main() {
     int arr[] = {2, 3, 5, 8, 10, 1, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
     int largest = large(arr, size);
+    int minimum = mini(arr, size);
     cout << "The largest element of the array is -->> " << largest << endl;
+    cout << "The Minimum element of the array is -->> " << minimum << endl;
+
     return 0;
 }
