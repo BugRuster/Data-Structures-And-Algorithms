61e2983d79c5f5708b9c65666a6edaa9b9184e28
anurag <90618167+BugRuster@users.noreply.github.com>
2023-02-10 12:49:46 +0530

    Maximum Subarray Sum by different Techniques

--- Coding/Arrays/MaximumSubarraySum.cpp
+++ Coding/Arrays/MaximumSubarraySum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void MaximuSubarraySum(int arr[], int size) {
+void MaximuSubarraySum(int arr[], int size) { // this is a brute force method with worst complexity of O(n^3)
     int MaxSum = INT16_MIN; // initilizing the maxsum to - infinity
     int CurrentSum = 0;
 
@@ -21,9 +21,48 @@ void MaximuSubarraySum(int arr[], int size) {
     }
     cout << MaxSum << endl;
 }
+
+void MaximuSubarraySum(int arr[], int size) {
+    int prefix[size];
+    int currSum = 0;  // This is a Prefix Method to find Maximum Subarray Sum
+    int MaxSum = INT16_MIN;
+    prefix[0] = arr[0];
+     for (int i = 1; i < size; i++) {
+        prefix[i] = prefix[i - 1] - prefix[i];
+     }
+
+     for (int i = 0; i < size; i++) {
+        int start = i;
+        for (int j = i; j < size; j++) {
+            int end = j;
+
+            currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
+
+            if (MaxSum < currSum) {
+                MaxSum = currSum;
+            }
+        }
+     }
+     cout << MaxSum << endl;
+}
+
+
+void MaximuSubarraySum(int arr[], int size) {
+    int mn = INT_MIN;
+    int cn = 0;
+    for (int i = 0; i < size; i++) {
+        cn = cn + arr[i];
+        if (cn < 0) {
+            cn = 0;  // Kadane's Algorithm
+        }
+        mn = max(cn, mn);
+    }
+    cout << mn << endl;
+}
+
 int main ()
 {
-    int arr[] = {2, 4, 6, 8, 10};
+    int arr[] = {1 ,-2, 6, -1 ,3};
     int size = sizeof(arr)/sizeof(arr[0]);
     MaximuSubarraySum(arr, size);
     return 0;
