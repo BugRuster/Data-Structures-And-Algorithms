623987acf320102201b1d8f823ac92752a28689c
anurag <90618167+BugRuster@users.noreply.github.com>
2023-02-15 10:45:14 +0530

    Sorting Updated with Practice Questions and their solutions

---
+++ Coding/Sorting/InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) { // starting from the index of 1.
+        int curr = arr[i];
+        int prev = i -1;
+        while (prev >= 0 && arr[prev] < curr) { // Updating The values till Last one 
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr; // Swaping the values 
+    }
+}
+
+void PrintArray(int arr[], int size) {
+    InsertionSort(arr, size);
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << endl;
+    }
+}
+
+int main ()
+{
+    int arr[] = {5, 4, 1, 3, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    PrintArray(arr, size);
+    return 0;
+}
+ 
+ 
+ 
+// @BugRuster
---
+++ Coding/Sorting/SelectionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < size - 1; j++) {
+            if (arr[min] < arr[j]) {
+                min = j;
+            }
+        }
+        // Swaping 
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        SelectionSort(arr, size);
+        cout << arr[i] << endl;
+    }
+}
+
+int main ()
+{
+    int arr[] = {5, 4, 1, 3, 2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    printArray(arr, size);
+    return 0;
+}
+ 
+ 
+ 
+// @BugRuster
